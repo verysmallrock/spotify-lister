9834202de1f7c0b8908557f3fc2587495d1f273f
Luke Rymarz <lukerymarz@gmail.com>
2021-01-02 14:50:00 -0800

    Make signin page look nicer

--- src/js/components/SigninView.js
+++ src/js/components/SigninView.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import S from './SigninView.css'
 
 export default class SigninView extends React.Component {
   render() {
-    return <div>
-      <h1>This is an example of the Authorization Code flow</h1>
-      <sp-button href="/login" variant="cta">Log in with Spotify</sp-button>
+    return <div className={ S.root }>
+      <div>
+        <h1>Spotify Song Finder</h1>
+        <sp-button href="/login" variant="cta">Log in with Spotify to continue</sp-button>
+      </div>
     </div>
   }
 }
